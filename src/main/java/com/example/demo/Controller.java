c8b2c65639f9214d003e036b815e80521e52b489
JasonKimyy <jason.t.kim2@gmail.com>
2025-10-14 10:43:32 -0700

    update branch

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,46 +1,29 @@
-package com.example.restapi;
+package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
 
 @RestController
 public class Controller {
 
-    @Value("${merriam.api.key}")
-    private String apiKey;
-
-    private static final List<String> WORDS = List.of(
-            "serendipity", "eloquent", "ephemeral", "luminous",
-            "benevolent", "melancholy", "labyrinth", "ineffable"
-    );
-
-    private final RestTemplate restTemplate = new RestTemplate();
-    private final Random random = new Random();
-
-    @GetMapping("/random-word")
-    public Map<String, Object> getRandomWordDefinition() {
-        // Pick a random word
-        String randomWord = WORDS.get(random.nextInt(WORDS.size()));
-
-        // Build the Merriam-Webster API URL
-        String url = String.format(
-                "https://www.dictionaryapi.com/api/v3/references/collegiate/json/%s?key=%s",
-                randomWord, apiKey
-        );
-
-        // Call the API
-        Object response = restTemplate.getForObject(url, Object.class);
-
-        // Return both the chosen word and the API response
-        return Map.of(
-                "word", randomWord,
-                "definition", response
-        );
+    @GetMapping("/")//Basic route for testing stuff
+    public String home(){
+        return "Hello World";
     }
+
+    //From here on will be routes that may be able to be used in theory,
+    //still trying to figure out where the API calls are in the front end
+    @GetMapping("/vocabList/?") //might not be needed as this may be handled in the app
+    public String VocabList(int userId){
+        return "Vocab List for: " + userId;
+    }
+
+    @GetMapping("/word_def/?")
+    public String word_def(String word) {
+        return word + ": def";
+    }
+
+    //Honestly, going to have to mess with this one a bunch
 }
