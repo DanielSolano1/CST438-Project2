77cc8d56d9f9b6bfe2db18e4cdba2a00fe1db017
lucybz <lucyblanco757@gmail.com>
2025-10-14 12:39:01 -0700

    Fix root mapping

--- src/main/java/com/example/demo/Group11Application.java
+++ src/main/java/com/example/demo/Group11Application.java
@@ -2,19 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class Group11Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Group11Application.class, args);
 	}
-	@GetMapping("/")
-	public String index() {
-		return "This is the root page";
-	}
+	//@GetMapping("/")
+	//public String index() {
+	//	return "This is the root page";
+	//}
 
 }
