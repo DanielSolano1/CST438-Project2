878ecdd1b255f4838b66ad83d8e2db6e46624ad3
lucybz <lucyblanco757@gmail.com>
2025-10-14 13:18:44 -0700

    Fix duplicate root mapping

--- src/main/java/com/example/demo/Group11Application.java
+++ src/main/java/com/example/demo/Group11Application.java
@@ -2,17 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class Group11Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Group11Application.class, args);
-	}
-	@GetMapping("/home")
-	public String index() {
-		return "This is the root page";
 	}
 
 }
