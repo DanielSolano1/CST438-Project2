6cbbfd9b8b1030cd4dcfcdde4d27057d84463369
lucybz <lucyblanco757@gmail.com>
2025-10-16 12:48:45 -0700

    Fix stage task for Heroku Gradle build

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -10,7 +10,7 @@ public class Controller {
 
     @GetMapping("/")//Basic route for testing stuff
     public String home(){
-        return "Hello World Good what";
+        return "Hello World Good whats good";
     }
 
     //From here on will be routes that may be able to be used in theory,
