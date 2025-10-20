75d3e76c2f91a6de7450e91ff5317bf37c0c2faf
lucybz <lucyblanco757@gmail.com>
2025-10-20 16:35:56 -0700

    test

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -10,7 +10,7 @@ public class Controller {
 
     @GetMapping("/")//Basic route for testing stuff
     public String home(){
-        return "Hello World Good whats good";
+        return "Hello World";
     }
 
     //From here on will be routes that may be able to be used in theory,
